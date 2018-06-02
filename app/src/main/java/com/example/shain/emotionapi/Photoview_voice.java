de5024f2c37ac0e6d5c608db03a47b76c5a3f1af
shain <shaing123@naver.com>
2018-06-02 20:58:59 +0900

    Add skeleton

---
+++ app/src/main/java/com/example/shain/emotionapi/Photoview_voice.java
@@ -0,0 +1,13 @@
+package com.example.shain.emotionapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Photoview_voice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photoview_voice);
+    }
+}
